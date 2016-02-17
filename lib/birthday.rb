967b3daffef821851983de6e3121d4f29aebc8c5
Brian Holland <brian.holland@proatria.com>
2016-02-17 15:00:21 -0600

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |key, val|
+    puts "Happy Birthday #{key.to_s.capitalize}! You are now #{val} years old!"
+  end
 end
 
 
