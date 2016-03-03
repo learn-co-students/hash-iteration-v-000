4658c87645c5c97c10a84331505f161960bf4dfb
tddycks <tadzio.smith@gmail.com>
2016-03-03 11:57:46 -0500

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name = "Amanda Presley"
+      winner = name
+    end
+  end
+  winner
 end
+
