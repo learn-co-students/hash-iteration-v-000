62f7506ab13db8c8f95556ad0fa25576a34c7e1c
bradleyoashburn <bradleyoashburn@gmail.com>
2016-03-04 22:53:04 -1000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,7 +5,12 @@
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
-
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each {|key, value| 
+    if key == :suite_a && value[0] == 'A'
+      winner = value
+    end
+  }
+  winner
 end
