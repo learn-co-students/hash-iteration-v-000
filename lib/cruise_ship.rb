3760233db27c2a525ca58713cd0029dcf1089b65
yeungn <nin.yeung@gmail.com>
2016-04-24 15:57:50 -0400

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,20 +1,4 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
-
 def select_winner(passengers)
-  passengers = {
-    suite_a: "Amanda Presley",
-    suite_b: "Seymour Hoffman",
-    suite_c: "Alfred Tennyson",
-    suite_d: "Charlie Chaplin",
-    suite_e: "Crumpet the Elf"
-  }
-
   winner = ""
 
   passengers.each do |suite, name|
