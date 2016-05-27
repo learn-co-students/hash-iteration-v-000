2005ec108ac0ecc355572217502d5eb9a1287797
vicision <duncan.j.mclachlan@gmail.com>
2016-05-27 14:18:01 -0400

    Done: all tests passing

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,14 +1,14 @@
 require_relative "../lib/birthday.rb"
 
-describe "#happy_birthday" do 
-  it "prints out a birthday message to each kid in the birthday_kids hash" do 
+describe "#happy_birthday" do
+  it "prints out a birthday message to each kid in the birthday_kids hash" do
     birthday_kids = {
-      "Timmy" => 9, 
-      "Sarah" => 6, 
+      "Timmy" => 9,
+      "Sarah" => 6,
       "Amanda" => 27
     }
 
-    expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
+    expect{happy_birthday(birthday_kids)}.to output("Happy birthday, Timmy! You are now 9 years old!\nHappy birthday, Sarah! You are now 6 years old!\nHappy birthday, Amanda! You are now 27 years old!\n").to_stdout
 
   end
 end
