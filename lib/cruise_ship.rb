50afc050bd56e89fd48dcb97164c6e28a68f2455
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-10 21:09:52 +0000

    after passing first test

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,9 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""    
+  passengers.each do |suite, name|
+     winner = name if suite == :suite_a && name[0] == "A" 
+  end
+  winner
 end
