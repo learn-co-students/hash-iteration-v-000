e1e597cd9ddc55f479765506ece2ab87b4795799
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-05-31 14:31:54 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -18,8 +18,4 @@ def select_winner(passengers)
   winner
 end
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
-  end
-end
+
