3d20156fffb0cd1d6fb60e86472116821238014c
Yehuda Makarov <yehudamakarov@gmail.com>
2017-08-02 18:55:38 -0400

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,37 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
+end
+
+# - without "puts" / for map- 2.4.1 :015 > select_winner(passengers)
+ # => ["Amanda Presley", nil, nil, nil, nil]
+#
+
+# - without "puts" / for map- 2.4.1 :015 > select_winner(passengers)
+ # => {:suite_a=>"Amanda Presley", :suite_b=>"Seymour Hoffman", :suite_c=>"Alfred Tennyson", :suite_d=>"Charlie Chaplin", :suite_e=>"Crumpet the Elf"}
+#
+
+birthday_kids = {
+	"Timmy" => 9,
+	"Sarah" => 6,
+	"Amanda" => 27
+}
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
