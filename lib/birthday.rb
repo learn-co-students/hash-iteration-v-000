f89f67e9a64a4840e30317e1ab66df92dcbb0313
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-09 19:44:14 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,13 +8,3 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each { |key, value| puts "Happy Birthday #{key}! You are now #{value} years old!" }
 end
-
-def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-  end
-end
-
-  
