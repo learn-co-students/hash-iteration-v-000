b69c4fdd56b85af802206616576f5e7549a27adc
Jonathan Dust <d24dust@hotmail.com>
2018-02-02 17:16:58 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,13 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    #puts "#{kids_name} aren't you a little old for chuck e cheese?" if age > 16
+  end
 end
-
-
