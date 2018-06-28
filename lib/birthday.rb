a0fde36e6ba0c6ae6477d1ad72b433b149c9ca6d
Alex Roman <elpitbull6989@gmail.com>
2018-06-28 08:15:07 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,8 @@
-# birthday_kids = {
-#   "Timmy" => 9,
-#   "Sarah" => 6,
-#   "Amanda" => 27
-# }
+ birthday_kids = {
+   "Timmy": 9,
+   "Sarah": 6,
+   "Amanda": 27
+ }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
@@ -11,3 +11,5 @@ def happy_birthday(birthday_kids)
 
   end
 end
+
+happy_birthday(birthday_kids)
