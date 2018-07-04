dfed1eedda495621acb79861d20afccb3e0a20a3
Angie Kassab <anasab12@gmail.com>
2018-07-04 23:44:58 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,21 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+}
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  hash = {"Timmy": 9, "Sarah": 6, "Amanda": 27}
+  
+  hash.each do |key, value|
+    puts "Happy Birthday " "#{key}! " "You are now " "#{value}" " years old!"
+  end
 end
 
+happy_birthday(birthday_kids)
 
+# hash = {key1: "value1", key2: "value2"}
+ 
+# hash.each do |key, value|
+#   puts "#{key}: #{value}"
+# end
