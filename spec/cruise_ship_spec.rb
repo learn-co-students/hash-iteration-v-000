c5093a3cf865a6e9d2cca271b2dea83a015e63a9
Kenzie Morlock <k.morlock86@gmail.com>
2018-08-18 23:28:49 +0000

    Done.

--- spec/cruise_ship_spec.rb
+++ spec/cruise_ship_spec.rb
@@ -2,16 +2,25 @@ require_relative "../lib/cruise_ship.rb"
 
 
 describe "#select_winner" do
-  it "returns the name of the passenger who stays in suite a and whose name begins with the letter 'A'" do 
+  it "returns the name of the passenger who stays in suite a and whose name begins with the letter 'A'" do
 
     passengers = {
-      suite_a: "Amanda Presley", 
-      suite_b: "Seymour Hoffman", 
-      suite_c: "Alfred Tennyson", 
-      suite_d: "Charlie Chaplin", 
+      suite_a: "Amanda Presley",
+      suite_b: "Seymour Hoffman",
+      suite_c: "Alfred Tennyson",
+      suite_d: "Charlie Chaplin",
       suite_e: "Crumpet the Elf"
       }
 
     expect(select_winner(passengers)).to eq("Amanda Presley")
   end
 end
+
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+
+winner  
