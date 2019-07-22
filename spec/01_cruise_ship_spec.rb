806c4bf71346ed8a47e9c61bd9157a4e4ee423b0
Tiffany <ong.tifffany1503@gmail.com>
2019-07-22 21:19:37 +0000

    Done.

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -5,10 +5,10 @@ describe "#select_winner" do
   it "returns the name of the passenger who stays in suite a and whose name begins with the letter 'A'" do 
 
     passengers = {
-      suite_a: "Amanda Presley", 
-      suite_b: "Seymour Hoffman", 
-      suite_c: "Alfred Tennyson", 
-      suite_d: "Charlie Chaplin", 
+      suite_a: "Amanda Presley",
+      suite_b: "Seymour Hoffman",
+      suite_c: "Alfred Tennyson",
+      suite_d: "Charlie Chaplin",
       suite_e: "Crumpet the Elf"
       }
 
